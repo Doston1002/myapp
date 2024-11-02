e8c975ee14a37859981a2b1c654707c4d851c0d8
doston1002 <polatovdoston1002@gmail.com>
2024-11-02 13:19:30 -0700

    login & register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+
+import { icon } from '../constants'
+import {Input} from '../ui/'
 const Register = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
   return (
-    <div>Register</div>
+    <main class="form-signin w-25 text-center m-auto mt-5">
+  <form>
+    <img class="mb-2" src={icon} alt="" width="72" height="60"/>
+    <h1 class="h3 mb-3 fw-normal">Please register</h1>
+
+    <Input label={'Username'} state={name} setState={setName}/>
+    <Input label={'Email address'} type={'email'} state={email} setState={setEmail}/>
+    <Input label={'Password'} type={'password'} state={password} setState={setPassword}/>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"/> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">© 2017–2022</p>
+  </form>
+</main>
   )
 }
 
---
+++ src/ui/Input.jsx
@@ -0,0 +1,10 @@
+const Input = ({label, state, type='text', setState}) => {
+  return (
+    <div class="form-floating">
+      <input type={type} class="form-control" onChange={e=>setState(e.target.value)} value={state} placeholder={label}/>
+      <label for="floatingInput">{label}</label>
+    </div>
+  )
+}
+
+export default Input
