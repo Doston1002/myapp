3c1757c51f35e387e786cbc6ad52c526ebabc621
doston1002 <polatovdoston1002@gmail.com>
2024-11-03 20:29:39 -0800

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { logo } from '../constants'
+import {useSelector} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {logo} from '../constants'
 
 const Navbar = () => {
-  return (
-    <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom pt-3">
-    <Link to={'/'}>
-    <img src={logo} alt="" />
-    </Link>
+	const {loggedIn, user} = useSelector(state => state.auth)
 
-      <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-        <Link className="me-3 py-2 text-dark text-decoration-none" to={'./login'}>Login</Link>
-        <Link className="me-3 py-2 text-dark text-decoration-none" to={'./register'}>Register</Link>
-      
-      </nav>
-    </div>
-  )
+	return (
+		<div className='d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom container pt-3'>
+			<Link to={'/'}>
+				<img src={logo} alt='' />
+			</Link>
+
+			<nav className='d-inline-flex mt-2 mt-md-0 ms-md-auto'>
+				{loggedIn ? (
+					<>
+						<p className='me-3 py-2 m-0 text-dark text-decoration-none'>{user.username}</p>
+						<button className='btn btn-outline-danger'>Logout</button>
+					</>
+				) : (
+					<>
+						<Link className='me-3 py-2 text-dark text-decoration-none' to={'/login'}>
+							Login
+						</Link>
+						<Link className='me-3 py-2 text-dark text-decoration-none' to={'/register'}>
+							Register
+						</Link>
+					</>
+				)}
+			</nav>
+		</div>
+	)
 }
 
 export default Navbar
